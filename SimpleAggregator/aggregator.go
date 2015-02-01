cd35ad4c414c0485a2c083821fb72bfabd3f3613
FumikoGlover <lezlie.cabellero_5002@yahoo.com>
2015-02-01 01:07:08 +0000

    simple aggregator in GO

---
+++ SimpleAggregator/aggregator.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "archive/zip"
+    "io"
+    "os"
+    "bytes"
+)
+
+func handler(resp http.ResponseWriter, req *http.Request) {
+    //query := req.URL.Query()
+    //deviceid := query.Get("devid")
+    body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        fmt.Println("Error: ", err)
+    }   
+    
+    r, err := zip.NewReader(bytes.NewReader(body), req.ContentLength)
+    if err != nil {
+        fmt.Println("Error: ", err)
+    }   
+    for _, zf := range r.File {
+        fmt.Println("filename: %s", zf.Name)
+        dst, err := os.Create(zf.Name)
+        if err != nil {
+            fmt.Println("Error: ", err)
+        }
+        defer dst.Close()
+        src, err := zf.Open()
+        if err != nil {
+            fmt.Println("Error: ", err)
+        }
+        defer src.Close()
+    
+        io.Copy(dst, src)
+    }   
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
