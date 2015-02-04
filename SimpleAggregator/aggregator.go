74f8d9fdc2f8ffe31514db7452fb675a265f7df7
FumikoGlover <lezlie.cabellero_5002@yahoo.com>
2015-02-04 01:33:39 +0000

    log file open mode: should create when there is none

--- SimpleAggregator/aggregator.go
+++ SimpleAggregator/aggregator.go
@@ -8,11 +8,13 @@ import (
     "os"
     "path"
     "bytes"
+    "log"
 )
 
 func Handler(resp http.ResponseWriter, req *http.Request) {
     query := req.URL.Query()
     deviceid := query.Get("devid")
+    log.Printf("Device id: %s", deviceid)
     if deviceid == "" {
         http.Error(resp, "devid is required parameter", http.StatusBadRequest)
         return
@@ -29,7 +31,8 @@ func Handler(resp http.ResponseWriter, req *http.Request) {
         http.Error(resp, err.Error(), http.StatusInternalServerError)
         return
     }   
-    dst, err := os.OpenFile(path.Join("logs",deviceid), os.O_APPEND|os.O_WRONLY,0600)
+    dst, err := os.OpenFile(path.Join("logs",deviceid), os.O_CREATE|os.O_APPEND|os.O_WRONLY,0600)
+    log.Printf("Appending to %s", dst)
     if err != nil {
         http.Error(resp, err.Error(), http.StatusInternalServerError)
         return
